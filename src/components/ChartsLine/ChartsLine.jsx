b5e932db48fe7d222f076d380be30945d5e366a2
Mykhailo Bodnar <mykhailo.bodnar@wetelo.com>
2023-11-09 12:21:54 +0100

    fix graph 1

--- src/components/ChartsLine/ChartsLine.jsx
+++ src/components/ChartsLine/ChartsLine.jsx
@@ -1,3 +1,95 @@
+// import React from 'react';
+// import {
+//     Chart as ChartJS,
+//     CategoryScale,
+//     LinearScale,
+//     PointElement,
+//     LineElement,
+//     Title,
+//     Tooltip,
+//     Legend,
+// } from 'chart.js';
+// import { Line } from 'react-chartjs-2';
+//
+// ChartJS.register(
+//     CategoryScale,
+//     LinearScale,
+//     PointElement,
+//     LineElement,
+//     Title,
+//     Tooltip,
+//     Legend
+// );
+//
+// export const ChartsLine = ({ mainGraph1, mainGraph2, mainGraph3, mainGraph4, mainGraph5, mainGraph6, formData }) => {
+//     const options = {
+//         maintainAspectRatio: false,
+//         responsive: true,
+//         plugins: {
+//             legend: {
+//                 position: 'top',
+//                 display: false,
+//             },
+//             title: {
+//                 display: false,
+//                 text: 'Chart.js Line Chart',
+//             },
+//         },
+//         scales: {
+//             x: {
+//                 display: false,
+//                 beginAtZero: true,
+//             },
+//             y: {
+//                 display: false,
+//             },
+//         },
+//     };
+//
+//     const labels = [1, 2, 3, 4, 5, 6];
+//
+//     const data = {
+//         labels,
+//         datasets: [
+//             {
+//                 label: 'MainGraph',
+//                 // data: [
+//                 //     customerData.mainGraph1,
+//                 //     customerData.mainGraph2,
+//                 //     customerData.mainGraph3,
+//                 //     customerData.mainGraph4,
+//                 //     customerData.mainGraph5,
+//                 //     customerData.mainGraph6,
+//                 // ],
+//                 // data: [
+//                 //     chartData.mainGraph1,
+//                 //     chartData.mainGraph2,
+//                 //     chartData.mainGraph3,
+//                 //     chartData.mainGraph4,
+//                 //     chartData.mainGraph5,
+//                 //     chartData.mainGraph6,
+//                 // ],
+//                 data: [
+//                     mainGraph1,
+//                     mainGraph2,
+//                     mainGraph3,
+//                     mainGraph4,
+//                     mainGraph5,
+//                     mainGraph6,
+//                 ],
+//                 pointRadius: 5,
+//                 pointBackgroundColor: '#4801E6',
+//                 pointBorderWidth: 1,
+//                 pointBorderColor: '#FF5C00',
+//                 borderColor: '#4801E6',
+//                 borderWidth: 1,
+//             },
+//         ],
+//     };
+//
+//     return <Line options={options} data={data}  />;
+// };
+
 import React from 'react';
 import {
     Chart as ChartJS,
@@ -21,7 +113,41 @@ ChartJS.register(
     Legend
 );
 
-export const ChartsLine = ({ ...customerData }) => {
+export const ChartsLine = ({ mainGraph1, mainGraph2, mainGraph3, mainGraph4, mainGraph5, mainGraph6 }) => {
+    const updateChartData = (data) => {
+        return {
+            labels: [1, 2, 3, 4, 5, 6],
+            datasets: [
+                {
+                    label: 'MainGraph',
+                    data: [
+                        data.mainGraph1,
+                        data.mainGraph2,
+                        data.mainGraph3,
+                        data.mainGraph4,
+                        data.mainGraph5,
+                        data.mainGraph6,
+                    ],
+                    pointRadius: 5,
+                    pointBackgroundColor: '#4801E6',
+                    pointBorderWidth: 1,
+                    pointBorderColor: '#FF5C00',
+                    borderColor: '#4801E6',
+                    borderWidth: 1,
+                },
+            ],
+        };
+    };
+
+    const data = updateChartData({
+        mainGraph1,
+        mainGraph2,
+        mainGraph3,
+        mainGraph4,
+        mainGraph5,
+        mainGraph6,
+    });
+
     const options = {
         maintainAspectRatio: false,
         responsive: true,
@@ -46,30 +172,5 @@ export const ChartsLine = ({ ...customerData }) => {
         },
     };
 
-    const labels = [1, 2, 3, 4, 5, 6];
-
-    const data = {
-        labels,
-        datasets: [
-            {
-                label: 'MainGraph',
-                data: [
-                    customerData.mainGraph1,
-                    customerData.mainGraph2,
-                    customerData.mainGraph3,
-                    customerData.mainGraph4,
-                    customerData.mainGraph5,
-                    customerData.mainGraph6,
-                ],
-                pointRadius: 5,
-                pointBackgroundColor: '#4801E6',
-                pointBorderWidth: 1,
-                pointBorderColor: '#FF5C00',
-                borderColor: '#4801E6',
-                borderWidth: 1,
-            },
-        ],
-    };
-
-    return <Line options={options} data={data}  />;
+    return <Line options={options} data={data} />;
 };
--- src/components/SingleCustomerDashboardInfo/SingleCustomerDashboardInfo.jsx
+++ src/components/SingleCustomerDashboardInfo/SingleCustomerDashboardInfo.jsx
@@ -1,13 +1,142 @@
-import React, {useState} from "react";
-import {DashboardContent} from "./Styles.jsx";
-import {ChartsLine} from "../ChartsLine/ChartsLine.jsx";
+// import React, {useState} from "react";
+// import {DashboardContent} from "./Styles.jsx";
+// import {ChartsLine} from "../ChartsLine/ChartsLine.jsx";
+//
+// export const SingleCustomerDashboardInfo = ({...customerData}) => {
+//     const [formData, setFormData] = useState({...customerData});
+//
+//     const [chartData, setChartData] = useState({
+//         mainGraph1: customerData.mainGraph1,
+//         mainGraph2: customerData.mainGraph2,
+//         mainGraph3: customerData.mainGraph3,
+//         mainGraph4: customerData.mainGraph4,
+//         mainGraph5: customerData.mainGraph5,
+//         mainGraph6: customerData.mainGraph6,
+//     });
+//
+//     const handleInputChange = (e) => {
+//         const { name, value } = e.target;
+//         setFormData((prevData) => ({
+//             ...prevData,
+//             [name]: value,
+//         }));
+//
+//         setChartData((prevChartData) => ({
+//             ...prevChartData,
+//             [name]: value,
+//         }));
+//     };
+//
+//
+//     // const handleInputChange = (e) => {
+//     //     const {name, value} = e.target;
+//     //     setFormData((prevData) => ({
+//     //         ...prevData,
+//     //         [name]: value,
+//     //     }));
+//     // };
+//
+//     const handleSaveClick = async () => {
+//         try {
+//             const accessToken = localStorage.getItem('accessToken');
+//             const response = await fetch('https://highdardata.xyz/office/v1/customers/setDashboard', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     Authorization: `Bearer ${accessToken}`,
+//                 },
+//                 body: JSON.stringify(formData),
+//             });
+//
+//             if (response.ok) {
+//                 console.log('Data saved successfully!');
+//
+//             } else {
+//                 console.error('Error saving customer data:', response.statusText);
+//             }
+//         } catch (error) {
+//             console.error('Error saving customer data:', error);
+//         }
+//     };
+//     return (
+//         <DashboardContent>
+//             <div className="control">
+//                 <h2>Dashboard</h2>
+//                 <button onClick={handleSaveClick}>
+//                     Save
+//                 </button>
+//             </div>
+//             <div className="dashboard-info-content">
+//                 <div className="dashboard-info">
+//                     <div className="dashboard-info-group">
+//                         <div>
+//                             Total Worth:
+//                             <input type="text" name="totalWorth" value={formData.totalWorth}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                         <div>
+//                             Difference:
+//                             <input type="text" name="differenceAmount" value={formData.differenceAmount}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                         <div>
+//                             Different in percent:
+//                             <input type="text" name="differencePercent" value={formData.differencePercent}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <div className="dashboard-info-group main-graph">
+//                     <h3>Graph1</h3>
+//                     <div className="items">
+//                         <div>
+//                             <input type="text" name="mainGraph1" value={formData.mainGraph1}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                         <div>
+//                             <input type="text" name="mainGraph2" value={formData.mainGraph2}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                         <div>
+//                             <input type="text" name="mainGraph3" value={formData.mainGraph3}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                         <div>
+//                             <input type="text" name="mainGraph4" value={formData.mainGraph4}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                         <div>
+//                             <input type="text" name="mainGraph5" value={formData.mainGraph5}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                         <div>
+//                             <input type="text" name="mainGraph6" value={formData.mainGraph6}
+//                                    onChange={handleInputChange}/>
+//                         </div>
+//                     </div>
+//                     {/*<div className="chart-line" style="height: 116px">*/}
+//                     {/*    <ChartsLine {...customerData}/>*/}
+//                     {/*</div>*/}
+//                     <div className="chart-line" style={{ height: '116px' }}>
+//                         <ChartsLine  chartData={chartData} />
+//                     </div>
+//
+//                 </div>
+//             </div>
+//         </DashboardContent>
+//     )
+// }
 
-export const SingleCustomerDashboardInfo = ({...customerData}) => {
-    const [formData, setFormData] = useState({...customerData});
 
+import React, { useState } from "react";
+import { DashboardContent } from "./Styles.jsx";
+import { ChartsLine } from "../ChartsLine/ChartsLine.jsx";
+
+export const SingleCustomerDashboardInfo = ({ ...customerData }) => {
+    const [formData, setFormData] = useState({ ...customerData });
 
     const handleInputChange = (e) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         setFormData((prevData) => ({
             ...prevData,
             [name]: value,
@@ -28,7 +157,15 @@ export const SingleCustomerDashboardInfo = ({...customerData}) => {
 
             if (response.ok) {
                 console.log('Data saved successfully!');
-
+                const updatedChartData = {
+                    mainGraph1: formData.mainGraph1,
+                    mainGraph2: formData.mainGraph2,
+                    mainGraph3: formData.mainGraph3,
+                    mainGraph4: formData.mainGraph4,
+                    mainGraph5: formData.mainGraph5,
+                    mainGraph6: formData.mainGraph6,
+                };
+                setChartData(updatedChartData);
             } else {
                 console.error('Error saving customer data:', response.statusText);
             }
@@ -36,7 +173,22 @@ export const SingleCustomerDashboardInfo = ({...customerData}) => {
             console.error('Error saving customer data:', error);
         }
     };
+
+    const [chartData, setChartData] = useState({
+        mainGraph1: customerData.mainGraph1,
+        mainGraph2: customerData.mainGraph2,
+        mainGraph3: customerData.mainGraph3,
+        mainGraph4: customerData.mainGraph4,
+        mainGraph5: customerData.mainGraph5,
+        mainGraph6: customerData.mainGraph6,
+    });
+
     return (
+        // <DashboardContent>
+        //     {/* ваш JSX-код */}
+        //     <ChartsLine {...chartData} />
+        // </DashboardContent>
+
         <DashboardContent>
             <div className="control">
                 <h2>Dashboard</h2>
@@ -92,12 +244,15 @@ export const SingleCustomerDashboardInfo = ({...customerData}) => {
                                    onChange={handleInputChange}/>
                         </div>
                     </div>
-                    <div className="chart-line" style="height: 116px">
-                        <ChartsLine {...customerData}/>
+                    {/*<div className="chart-line" style="height: 116px">*/}
+                    {/*    <ChartsLine {...customerData}/>*/}
+                    {/*</div>*/}
+                    <div className="chart-line" style={{ height: '116px' }}>
+                        <ChartsLine {...chartData} />
                     </div>
 
                 </div>
             </div>
         </DashboardContent>
-    )
-}
+    );
+};
