636070ea1b661a7f6a9aea4b97818007d004a153
Mykhailo Bodnar <mykhailo.bodnar@wetelo.com>
2023-11-10 13:18:07 +0100

    added edit asset

--- src/components/AdminAssetTable/AdminAssetTable.jsx
+++ src/components/AdminAssetTable/AdminAssetTable.jsx
@@ -1,3 +1,166 @@
+// import { AdminAssetTableContent } from "./Styles.jsx";
+// import edit from "../../assets/images/edit.svg";
+// import bin from "../../assets/images/bin.svg";
+// import React, { useEffect, useState } from "react";
+// import Modal from "../Modal/Modal.jsx";
+// import EditAdminAssetModalContent from "../EditAdminAssetModalContent/EditAdminAssetModalContent.jsx";
+// import CreateAdminAssetModalContent from "../CreateAdminAssetModalContent/CreateAdminAssetModalContent.jsx";
+//
+// const useSingleCustomer = (id) => {
+//     const [customerData, setCustomerData] = useState(null);
+//
+//     const fetchData = async () => {
+//         try {
+//             const accessToken = localStorage.getItem('accessToken');
+//             const response = await fetch(`https://highdardata.xyz/office/v1/customers/getSingle?id=${id}`, {
+//                 headers: {
+//                     Authorization: `Bearer ${accessToken}`,
+//                 },
+//             });
+//
+//             if (response.ok) {
+//                 const data = await response.json();
+//                 setCustomerData(data);
+//             } else {
+//                 console.error('Error fetching customer data:', response.statusText);
+//             }
+//         } catch (error) {
+//             console.error('Error fetching customer data:', error);
+//         }
+//     };
+//
+//     useEffect(() => {
+//         if (id) {
+//             fetchData();
+//             console.log("дані оновлено")
+//         }
+//
+//         return () => {
+//             // Очищення даних або виконання інших дій, які потрібно зробити при видаленні компонента
+//         };
+//     }, [id]);
+//
+//     return { customerData, fetchData };
+// };
+//
+// export const AdminAssetTable = ({ isAssetCreateModalOpen, setIsAssetCreateModalOpen, onCreate }) => {
+//     const [error, setError] = useState(null);
+//     const cryptoIconPath = '/src/assets/images/cryptoIcon/';
+//
+//
+//     const searchParams = new URLSearchParams(window.location.search);
+//     const id = searchParams.get('id');
+//
+//     const { customerData, fetchData } = useSingleCustomer(id);
+//
+//     const assets = customerData?.assets || [];
+//
+//     const handleDeleteAsset = async (assetId) => {
+//         console.log(assetId);
+//         try {
+//             const accessToken = localStorage.getItem('accessToken');
+//             const response = await fetch(`https://highdardata.xyz/office/v1/assets/delete?id=${assetId}`, {
+//                 headers: {
+//                     'Authorization': `Bearer ${accessToken}`
+//                 }
+//             });
+//
+//             if (response.ok) {
+//                 console.log("Asset successfully deleted");
+//                 await fetchData();
+//                 console.log("after fetchData")
+//             } else {
+//                 setError('Error deleting asset');
+//             }
+//         } catch (error) {
+//             setError('Error deleting asset');
+//         }
+//     };
+//
+//     const [isAssetEditModalOpen, setIsAssetEditModalOpen] = useState(false);
+//
+//     const openEditAssetModal = () => {
+//         setIsAssetEditModalOpen(true);
+//     };
+//
+//     const closeEditAssetModal = () => {
+//         setIsAssetEditModalOpen(false);
+//     };
+//
+//     const handleEditAsset = (formData) => {
+//         openEditAssetModal()
+//     };
+//
+//     const handleCreateAsset = (formData) => {
+//          fetchData();
+//         setIsAssetCreateModalOpen(false);
+//     };
+//
+//     return (
+//         <AdminAssetTableContent>
+//             <table>
+//                 <thead>
+//                 <tr>
+//                     <th>Name</th>
+//                     <th>Amount</th>
+//                     <th>Price</th>
+//                     <th>Logo</th>
+//                     <th>Action</th>
+//                     <th></th>
+//                 </tr>
+//                 </thead>
+//                 <tbody>
+//                 {assets.map((asset) => (
+//                     <tr key={asset.id}>
+//                         <td>{asset.name}</td>
+//                         <td>{asset.amount}</td>
+//                         <td>{asset.price}</td>
+//                         <td>
+//                             <img className="coin-icon" src={`${cryptoIconPath}${asset.logoName}`} alt="" />
+//                         </td>
+//                         <td>
+//                             <img
+//                                 className="edit"
+//                                 src={edit}
+//                                 alt="icon"
+//                                 onClick={() => {
+//                                     handleEditAsset(asset.id);
+//                                 }}
+//                             />
+//                         </td>
+//                         <td>
+//                             <img className="bin"
+//                                  src={bin}
+//                                  alt="icon"
+//                                  onClick={() => handleDeleteAsset(asset.id)}
+//                             />
+//                         </td>
+//                     </tr>
+//                 ))}
+//                 </tbody>
+//             </table>
+//             {isAssetEditModalOpen &&  (
+//                 <Modal isOpen={setIsAssetEditModalOpen} onClose={() => setIsAssetEditModalOpen(false)}>
+//                     <EditAdminAssetModalContent onSave={fetchData} isOpen={isAssetEditModalOpen} onClose={closeEditAssetModal} onCreate={handleEditAsset} />
+//                 </Modal>
+//             )}
+//             {isAssetCreateModalOpen && (
+//                 <Modal isOpen={setIsAssetCreateModalOpen} onClose={() => setIsAssetCreateModalOpen(false)}>
+//                     <CreateAdminAssetModalContent
+//                         isOpen={isAssetCreateModalOpen}
+//                         onClose={() => setIsAssetCreateModalOpen(false)}
+//                         onCreate={handleCreateAsset}
+//                     />
+//                 </Modal>
+//             )}
+//         </AdminAssetTableContent>
+//     )
+// }
+//
+//
+//
+
+
 import { AdminAssetTableContent } from "./Styles.jsx";
 import edit from "../../assets/images/edit.svg";
 import bin from "../../assets/images/bin.svg";
@@ -47,7 +210,6 @@ export const AdminAssetTable = ({ isAssetCreateModalOpen, setIsAssetCreateModalO
     const [error, setError] = useState(null);
     const cryptoIconPath = '/src/assets/images/cryptoIcon/';
 
-
     const searchParams = new URLSearchParams(window.location.search);
     const id = searchParams.get('id');
 
@@ -78,6 +240,7 @@ export const AdminAssetTable = ({ isAssetCreateModalOpen, setIsAssetCreateModalO
     };
 
     const [isAssetEditModalOpen, setIsAssetEditModalOpen] = useState(false);
+    const [editAssetData, setEditAssetData] = useState(null);
 
     const openEditAssetModal = () => {
         setIsAssetEditModalOpen(true);
@@ -87,12 +250,18 @@ export const AdminAssetTable = ({ isAssetCreateModalOpen, setIsAssetCreateModalO
         setIsAssetEditModalOpen(false);
     };
 
-    const handleEditAsset = (formData) => {
-        openEditAssetModal()
+    const handleSetEditAssetData = (data) => {
+        setEditAssetData(data);
+    };
+
+    const handleEditAsset = (assetId) => {
+        const selectedAsset = assets.find(asset => asset.id === assetId);
+        handleSetEditAssetData(selectedAsset);
+        setIsAssetEditModalOpen(true);
     };
 
     const handleCreateAsset = (formData) => {
-         fetchData();
+        fetchData();
         setIsAssetCreateModalOpen(false);
     };
 
@@ -139,9 +308,15 @@ export const AdminAssetTable = ({ isAssetCreateModalOpen, setIsAssetCreateModalO
                 ))}
                 </tbody>
             </table>
-            {isAssetEditModalOpen &&  (
+            {isAssetEditModalOpen && (
                 <Modal isOpen={setIsAssetEditModalOpen} onClose={() => setIsAssetEditModalOpen(false)}>
-                    <EditAdminAssetModalContent onSave={fetchData} isOpen={isAssetEditModalOpen} onClose={closeEditAssetModal} onCreate={handleEditAsset} />
+                    <EditAdminAssetModalContent
+                        onSave={fetchData}
+                        isOpen={isAssetEditModalOpen}
+                        onClose={closeEditAssetModal}
+                        onCreate={handleEditAsset}
+                        editAssetData={editAssetData}
+                    />
                 </Modal>
             )}
             {isAssetCreateModalOpen && (
@@ -154,8 +329,6 @@ export const AdminAssetTable = ({ isAssetCreateModalOpen, setIsAssetCreateModalO
                 </Modal>
             )}
         </AdminAssetTableContent>
-    )
-}
-
-
+    );
+};
 
--- src/components/EditAdminAssetModalContent/EditAdminAssetModalContent.jsx
+++ src/components/EditAdminAssetModalContent/EditAdminAssetModalContent.jsx
@@ -1,52 +1,150 @@
+//
+// import React, {useState} from 'react';
+// import {ModalContent} from "./Styles.jsx";
+//
+// const EditAdminAssetModalContent = ({isOpen, onClose, onCreate, onSave}) => {
+//
+//     const [formData, setFormData] = useState({
+//         id: '',
+//         name: '',
+//         amount: '',
+//         price: '',
+//         logoName: 'bitcoin.svg',
+//     });
+//
+//     const handleInputChange = (e) => {
+//         const {name, value} = e.target;
+//         setFormData({...formData, [name]: value});
+//     };
+//
+//     const handleSave = async () => {
+//         try {
+//             const accessToken = localStorage.getItem('accessToken');
+//             const response = await fetch('https://highdardata.xyz/office/v1/assets/update', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json',
+//                     Authorization: `Bearer ${accessToken}`,
+//                 },
+//                 body: JSON.stringify(formData),
+//             });
+//
+//             if (response.ok) {
+//                 onCreate(formData);
+//                 console.log('Asset created successfully!-00');
+//
+//                 onSave();
+//                 console.log('after create onSave fetchData')
+//                 onClose();
+//             } else {
+//                 console.error('Помилка при відправці даних на сервер');
+//             }
+//         } catch (error) {
+//             console.error('Помилка:', error);
+//         }
+//     };
+//
+//     if (!isOpen) {
+//         return null;
+//     }
+//
+//     return (
+//         <ModalContent>
+//             <div className="modal-content">
+//                 <div className="create-assets-modal-fields">
+//                     <div>
+//                         <label>Name:</label>
+//                         <input type="text" name="name" value={formData.name} onChange={handleInputChange}/>
+//                     </div>
+//                     <div>
+//                         <label>Amount:</label>
+//                         <input type="text" name="amount" value={formData.amount} onChange={handleInputChange}/>
+//                     </div>
+//                     <div>
+//                         <label>Price:</label>
+//                         <input type="text" name="price" value={formData.price} onChange={handleInputChange}/>
+//                     </div>
+//                     <div>
+//                         <label>Logo:</label>
+//                         {/*<input type="text" name="logoName" value={formData.logoName} onChange={handleInputChange}/>*/}
+//                         <select name="logoName" value={formData.logoName} onChange={handleInputChange}>
+//                             <option value="bitcoin.svg"> Bitcoin</option>
+//                             <option value="usdt.svg">Tether</option>
+//                             <option value="ethereum.svg">Ethereum</option>
+//                             <option value="litecoin.svg">Litecoin</option>
+//                         </select>
+//                     </div>
+//                 </div>
+//                 <button onClick={handleSave}>Save</button>
+//             </div>
+//
+//
+//         </ModalContent>
+//     );
+// };
+//
+// export default EditAdminAssetModalContent;
+//
+//
 
-import React, {useState} from 'react';
-import {ModalContent} from "./Styles.jsx";
 
-const EditAdminAssetModalContent = ({isOpen, onClose, onCreate, onSave}) => {
+// EditAdminAssetModalContent.jsx
 
+import React, { useState } from "react";
+import { ModalContent } from "./Styles.jsx";
+
+const EditAdminAssetModalContent = ({ onSave, onClose, onCreate, editAssetData }) => {
     const [formData, setFormData] = useState({
-        id: '',
-        name: '',
-        amount: '',
-        price: '',
-        logoName: 'bitcoin.svg',
+        // Initialize form data with existing asset data
+        id: editAssetData?.id,
+        name: editAssetData?.name || "",
+        amount: editAssetData?.amount || "",
+        price: editAssetData?.price || "",
+        logoName: editAssetData?.logoName || "",
+        // Add other form fields as needed
     });
 
-    const handleInputChange = (e) => {
-        const {name, value} = e.target;
-        setFormData({...formData, [name]: value});
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
     };
 
     const handleSave = async () => {
         try {
             const accessToken = localStorage.getItem('accessToken');
-            const response = await fetch('https://highdardata.xyz/office/v1/assets/update', {
-                method: 'POST',
+            const response = await fetch("https://highdardata.xyz/office/v1/assets/update", {
+                method: "POST",
                 headers: {
-                    'Content-Type': 'application/json',
+                    "Content-Type": "application/json",
                     Authorization: `Bearer ${accessToken}`,
                 },
                 body: JSON.stringify(formData),
             });
 
             if (response.ok) {
-                onCreate(formData);
-                console.log('Asset created successfully!-00');
-
+                console.log("Asset successfully updated");
+                // Call onSave function if needed
                 onSave();
-                console.log('after create onSave fetchData')
+                // Close the modal
                 onClose();
             } else {
-                console.error('Помилка при відправці даних на сервер');
+                console.error("Error updating asset:", response.statusText);
             }
         } catch (error) {
-            console.error('Помилка:', error);
+            console.error("Error updating asset:", error);
         }
     };
 
-    if (!isOpen) {
-        return null;
-    }
+    const handleCreate = () => {
+        // Perform create operation with formData
+        // Call onCreate function if needed
+        onCreate(formData);
+        // Close the modal
+        onClose();
+    };
 
     return (
         <ModalContent>
@@ -54,20 +152,37 @@ const EditAdminAssetModalContent = ({isOpen, onClose, onCreate, onSave}) => {
                 <div className="create-assets-modal-fields">
                     <div>
                         <label>Name:</label>
-                        <input type="text" name="name" value={formData.name} onChange={handleInputChange}/>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div>
                         <label>Amount:</label>
-                        <input type="text" name="amount" value={formData.amount} onChange={handleInputChange}/>
+                        <input
+                            type="text"
+                            id="amount"
+                            name="amount"
+                            value={formData.amount}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div>
                         <label>Price:</label>
-                        <input type="text" name="price" value={formData.price} onChange={handleInputChange}/>
+                        <input
+                            type="text"
+                            id="price"
+                            name="price"
+                            value={formData.price}
+                            onChange={handleChange}
+                        />
                     </div>
                     <div>
                         <label>Logo:</label>
-                        {/*<input type="text" name="logoName" value={formData.logoName} onChange={handleInputChange}/>*/}
-                        <select name="logoName" value={formData.logoName} onChange={handleInputChange}>
+                        <select name="logoName" id="logoName" value={formData.logoName} onChange={handleChange}>
                             <option value="bitcoin.svg"> Bitcoin</option>
                             <option value="usdt.svg">Tether</option>
                             <option value="ethereum.svg">Ethereum</option>
@@ -77,12 +192,9 @@ const EditAdminAssetModalContent = ({isOpen, onClose, onCreate, onSave}) => {
                 </div>
                 <button onClick={handleSave}>Save</button>
             </div>
-
-
         </ModalContent>
     );
 };
 
 export default EditAdminAssetModalContent;
-
 
